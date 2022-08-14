bd7223aa17e20aa9dcd7721f0fdfc4bbeafcafbd
SarahKirksey <sarahbeatricedanielle@gmail.com>
2022-08-13 21:24:01 -0700

    Update randcall.js

--- modules/randcall.js
+++ modules/randcall.js
@@ -1,4 +1,6 @@
 
+const bonus = [[1,2],[1,4],[1,6],[1,8],[1,10],[1,12],[2,16],[2,20],[2,24],[3,30],[3,36],[4,40],[5,50],[6,60],[7,70],[8,80],[10,100]];
+
 
 exports.randLessThan = function(upperLimit){
 	return randLessThan(upperLimit);
@@ -16,12 +18,26 @@ function rollXdY(x, y){
 	return retVal + x;
 }
 
+function roll1dX(x){
+	return randLessThan(x) + 1;
+}
+
 function rollXdYZTimes(x, y, z){
 	let retVal = [];
 	for(let i=0; i<z; i++){
 		retVal.push(rollXdY(x, y));
 	}
 	return retVal;
+}
+
+exports.rollBonusUsingOldMethod(tier){
+	let bonusRoll = bonus[tier];
+	return Math.floor((Math.random() * (bonusRoll[1] - bonusRoll[0])) + bonusRoll[0]);
+}
+
+exports.rollBonusUsingDice(tier){
+	let bonusRoll = bonus[tier];
+	return rollXdY(bonusRoll[0], bonusRoll[1]/bonusRoll[0]);
 }
 
 exports.rollXdY = function(x,y){
@@ -35,8 +51,15 @@ exports.roll1dX = function(x){
 	return Math.ceil(Math.random() * x) || x;
 }
 
-exports.rollToHit = function(noirBonus = false, refinedBonus = false){
-	let retVal = (noirBonus == true) ? 
+exports.rollToHit = function(noirBonus = false, refinedBonus = false, scienceBonus = false){
+	let retVal = [roll1dX(20), (noirBonus == true) ? roll1dX(4) : 0];
+	if(refinedBonus == true){
+		retVal[1] += 2;
+		if(scienceBonus == true){
+			retVal[1] += 2;
+		}
+	}
+	return retVal;
 }
 
 exports.tileToLocal = function(client,message,tile){
