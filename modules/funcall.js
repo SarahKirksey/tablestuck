e26219fa5d92cede210c08bb94f45239cbb41c55
SarahKirksey <sarahbeatricedanielle@gmail.com>
2022-08-24 10:50:34 -0700

    Build-time error fixes for Hacky Worldgen

--- modules/funcall.js
+++ modules/funcall.js
@@ -638,7 +638,7 @@ function actOnActionList(client, message, charid, tile, roomIndex, map, actionLi
 			console.log(`LOOT_S trigger was unable to glean sec from designation "${map}"`);
 			break;
 		}
-		tile[2][roomIndex][5] = [client.lootcall.lootA(client, sec, client.randcall.rollXdY(2,8) - 2];
+		tile[2][roomIndex][5] = [client.lootcall.lootA(client, sec, client.randcall.rollXdY(2,8) - 2)];
 
 		removeFromTriggers.push([tile[2][roomIndex][1].onSomeoneEnterRoom]);
 		break;
@@ -649,7 +649,7 @@ function actOnActionList(client, message, charid, tile, roomIndex, map, actionLi
 			console.log(`LOOT_B trigger was unable to glean sec from designation "${map}"`);
 			break;
 		}
-		tile[2][roomIndex][5] = [client.lootcall.lootB(client, sec, client.randcall.rollXdY(2,8) - 2];
+		tile[2][roomIndex][5] = [client.lootcall.lootB(client, sec, client.randcall.rollXdY(2,8) - 2)];
 
 		removeFromTriggers.push([tile[2][roomIndex][1].onSomeoneEnterRoom]);
 		break;
