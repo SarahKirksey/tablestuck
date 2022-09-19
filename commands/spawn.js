2ba89f44e8b5902034d1d26c7759cf66762ce4c7
SarahKirksey <sarahbeatricedanielle@gmail.com>
2022-09-18 19:26:32 -0700

    Project Black: Combat corrections

--- commands/spawn.js
+++ commands/spawn.js
@@ -14,8 +14,21 @@ message.channel.send(`Please select what you want to spawn, such as \"${client.a
 return;
 }
 
-let spawnList = ["imp","ogre","basilisk","lich","giclopse","titachnid","denizen"];
-if(!spawnList.includes(args[0].toLowerCase())){
+args[0] = args[0].toLowerCase();
+
+switch(args[0]){
+  case "ss": args[1] = "royal"; args[0] = "Sovereign Slayer"; break;
+  case "dd": args[1] = "royal"; args[0] = "Draconian Dignitary"; break;
+  case "hb": args[1] = "royal"; args[0] = "Hegemonic Brute"; break;
+  case "cd": args[1] = "royal"; args[0] = "Courtyard Droll"; break;
+  case "queen": args[1] = "royal"; break;
+  case "king": args[1] = "royal"; break;
+  case "bq": args[1] = "royal"; args[0] = "queen"; break;
+  case "bk": args[1] = "royal"; args[0] = "king"; break;
+}
+
+let spawnList = ["imp","ogre","basilisk","lich","giclopse","titachnid","denizen","queen","king", "Draconian Dignitary", "Sovereign Slayer", "Hegemonic Brute", "Courtyard Droll"];
+if(!spawnList.includes(args[0])){
 let msg = `Sorry, your choice isn't recognized. The current spawns supported are:`;
 for(let i=0;i<spawnList.length;i++){
   msg+=`\n${spawnList[i]}`;
@@ -23,10 +36,10 @@ for(let i=0;i<spawnList.length;i++){
 message.channel.send(msg);
 return;
 }
-let gristList = ["uranium","amethyst","garnet","iron","marble","chalk","shale","cobalt","ruby","caulk","tar","amber"];
+let gristList = ["uranium","amethyst","garnet","iron","marble","chalk","shale","cobalt","ruby","caulk","tar","amber","royal"];
 if(args[1]&&!gristList.includes(args[1].toLowerCase())){
  let msg = `Sorry, that's not a valid grist type. The availible grist types are:\n`;
- for (let i=0;i<gristList.length;i++){
+ for (let i=0;i<gristList.length-1;i++){
    msg+=`${gristList[i]}, `;
  }
  message.channel.send(msg);
@@ -34,9 +47,12 @@ if(args[1]&&!gristList.includes(args[1].toLowerCase())){
 }
 let undername;
 if(!args[1]){
-undername = client.strifecall.spawn(client,message,args[0].toLowerCase())
+undername = client.strifecall.spawn(client,message,args[0])
 } else {
-undername = client.strifecall.spawn(client,message,args[0].toLowerCase(),args[1].toLowerCase());
+undername = client.strifecall.spawn(client,message,args[0],args[1].toLowerCase());
+}
+if(args[1] === "royal"){
+  undername = args[0];
 }
 message.channel.send(`Spawned ${undername} in current room!`);
 }
